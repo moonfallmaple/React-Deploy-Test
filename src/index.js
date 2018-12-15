23c3b885eb5b8094c8bc4b431a29446f88273a2d
moonfalljade <moonfalljade@hotmail.com>
2018-12-15 13:06:30 +0800

    none

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-class Header extends Component {
-  render () {
-    return (
-     React.createElement(
-        "div",
-        null,
-        React.createElement(
-          "h1",
-          { className: 'title' },
-          "React 小书"
-        )
-      )
-    )
-  }
-}
-
-ReactDOM.render(
-  React.createElement(Header, null), 
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
