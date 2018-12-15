d5285bd77955d4a52295a5229848c53e614d6369
moonfalljade <moonfalljade@hotmail.com>
2018-12-15 11:22:37 +0800

    none

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hello world!</h1>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to fuck.
           </p>
           <a
             className="App-link"
